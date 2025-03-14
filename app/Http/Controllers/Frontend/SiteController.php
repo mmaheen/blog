ddf0e4b27b89cfafd714b96483438cf0ed20ff80
mmaheen <mushrifmaheen@gmail.com>
2025-03-14 10:39:37 +0600

    UserSeeder

--- app/Http/Controllers/Frontend/SiteController.php
+++ app/Http/Controllers/Frontend/SiteController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use Exception;
+
+use App\Models\User;
+
 class SiteController extends Controller
 {
     //
@@ -20,18 +24,39 @@ class SiteController extends Controller
         // dd ($request);
         // dd ($request->file());
         
-        $request->validate([
-            'name'=>'required',
-            'email'=>'required|email',
-            'password'=>'required|min:2|max:20',
-            'photo'=>'required|image'
-        ]);
-        session()->flash('message','User registration success');
-        return redirect()->back();
-
-        $photo=$request->file();
-        $file_name=rand(1111,9999).date('Y-m-d_H-i-s').$photo->getClientOriginalExtension();
-        $photo->storeAs('images',$file_name);
+        try{
+            $request->validate([
+                'name'=>'required',
+                'email'=>'required|email|unique',
+                'password'=>'required|min:2|max:20',
+                'photo'=>'required|image'
+            ],[
+                'name.required'=>'Please enter your name',
+                'email.required'=>'Please provide your email address'
+            ]);
+    
+            // $photo=$request->file();
+            // $file_name=rand(1111,9999).date('Y-m-d_H-i-s').$photo->getClientOriginalExtension();
+            // $photo->storeAs('images',$file_name);
+           
+            // $user=new User();
+            // $user->name=$request->name;
+            // $user->email=$request->email;
+            // // $user->password=$request->password;
+            // $user->password=bcrypt($request->password);
+            // $user->save();
+    
+            User::create([
+                'name'=>$request->name,
+                'email'=>$request->email,
+                'password'=>$request->password,
+            ]);
+            session()->flash('message','User registration success');
+        }
+        catch(Exception $error){
+            dd($error->getMessage());
+        }
+        // return redirect()->back();
     }
 
     public function showLoginForm(){
