e98f5acede1df16931a367227123e07c3551c678
mmaheen <mushrifmaheen@gmail.com>
2025-04-21 18:56:04 +0600

    Resource Route array

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\Frontend\SiteController;
 
 Route::get('/',[SiteController::class,'index'])->name('index');
@@ -17,6 +19,10 @@ Route::prefix('admin')->name('admin.')->group(function(){
     Route::get('/dashboard',[App\Http\Controllers\Backend\DashboardController::class,'index'])->name('dashboard');
     Route::get('/registration',[App\Http\Controllers\Backend\DashboardController::class,'showRegistrationForm'])->name('registrationform');
     Route::post('/registration',[App\Http\Controllers\Backend\DashboardController::class,'registration'])->name('registration');
-    Route::resource('categories',App\Http\Controllers\CategoryController::class);
-    Route::resource('post',App\Http\Controllers\PostController::class);
+    // Route::resource('categories',App\Http\Controllers\CategoryController::class);
+    // Route::resource('post',App\Http\Controllers\PostController::class);
+    Route::resources([
+        'categories'=>CategoryController::class,
+        'post'=>PostController::class
+    ]);
 });
