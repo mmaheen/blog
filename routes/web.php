a88bec9327d380c0feeba357b033531e03a4837b
mmaheen <mushrifmaheen@gmail.com>
2025-04-23 02:47:40 +0600

    Middleware group

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,8 @@ Route::prefix('user')->name('user.')->group(function(){
 
 });
 
-Route::prefix('admin')->name('admin.')->group(function(){
-    Route::get('/dashboard',[App\Http\Controllers\Backend\DashboardController::class,'index'])->name('dashboard')->middleware('ValidUser');
+Route::prefix('admin')->name('admin.')->middleware('ValidUser')->group(function(){
+    Route::get('/dashboard',[App\Http\Controllers\Backend\DashboardController::class,'index'])->name('dashboard');
     Route::get('/registration',[App\Http\Controllers\Backend\DashboardController::class,'showRegistrationForm'])->name('registrationform');
     Route::post('/registration',[App\Http\Controllers\Backend\DashboardController::class,'registration'])->name('registration');
     // Route::resource('categories',App\Http\Controllers\CategoryController::class);
