797650f9c7509405866400440684792453d0d5fb
mmaheen <mushrifmaheen@gmail.com>
2025-04-23 03:42:26 +0600

    Bug fix

--- resources/views/frontend/postshow.blade.php
+++ resources/views/frontend/postshow.blade.php
@@ -13,7 +13,7 @@
             <div class="card-body">
                 <div class="small text-muted">{{$post->created_at}}</div>
                 <h2 class="card-title h4">{{$post->name}}</h2>
-                <h4 class="card-subtitle h4"><a href="{{route('admin.categories.show',$post->category->id)}}" class="h4">{{$post->category->name}}</a></h4>
+                <h4 class="card-subtitle h4"><a href="{{route('index.category.show',$post->category->id)}}" class="h4">{{$post->category->name}}</a></h4>
                 <p class="card-text">{{$post->description}}</p>
                 
             </div>
